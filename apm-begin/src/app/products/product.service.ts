03cd3905d655c564f59f557ff0248224bead8aa7
Mar Andres <mar.andreslopez@axpo.com>
2024-08-08 17:24:13 +0200

    Use switchMap for dealing with inner observables

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from './product';
-import { catchError, Observable, tap, of, throwError } from 'rxjs';
-import { ProductData } from './product-data';
+import { catchError, Observable, tap, of, map, throwError, switchMap } from 'rxjs';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { Review } from '../reviews/review';
+import { ReviewService } from '../reviews/review.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -26,9 +28,20 @@ export class ProductService {
   getProduct(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.productsUrl}/${id}`)
       .pipe(
-        tap(() => console.log('In http.get one product by id pipeline')),
+        switchMap(product => this.getProductWithReviews(product)),
         catchError(err => this.handleError(err)),
       );
+  }
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if(product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+      .pipe(
+        map(reviews => ({...product, reviews} as Product)),
+      )
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
