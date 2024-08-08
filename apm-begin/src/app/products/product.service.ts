86a68ccdf1fa8852c04f94dd6779dc14f2aa2577
Mar Andres <mar.andreslopez@axpo.com>
2024-08-08 14:14:47 +0200

    retrieve an array of items in a service

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from './product';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,12 @@ export class ProductService {
   // Just enough here for the code to compile
   private productsUrl = 'api/products';
 
+  private http = inject(HttpClient);
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        tap(() => console.log('In http.get pipeline')),
+      );
+  }
 }
